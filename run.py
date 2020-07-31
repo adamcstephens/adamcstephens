601faf8f3aca1e89c31e7b71f5211c051c4a4aa0
Adam Stephens <adam@valkor.net>
2020-07-30 22:52:57 -0400

    prettify the readme some

--- run.py
+++ run.py
@@ -8,11 +8,13 @@ with open("README.md", "w") as readme:
     readme.write(
         """# 🌟🌟🌟🌟🌟
 
+These are my most recent stars, [updated daily](.github/workflows/readme.yml).
+
 """
     )
 
     for repo in gh.get_user().get_starred()[:5]:
-        readme.write("* " + repo.name + "\n")
-        print(repo.name)
+        readme.write("* [" + repo.full_name + "](" + repo.html_url + ")\n")
+        print(":: " + repo.full_name)
 
     readme.write("\nLast updated: " + datetime.utcnow().isoformat() + "\n")
